8fc4b207813aeb327c5758223d548abd0c68c55f
Abhishek Shaw <165417145+akshawop@users.noreply.github.com>
2024-05-11 10:43:01 +0530

    Create keyboard functionality.js

---
+++ keyboard functionality.js
@@ -0,0 +1,19 @@
+// defining the valid keys to be pressed
+const validKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '(', ')', '/', '*', '-', '+'];
+
+
+// Event listener for key press
+document.addEventListener("keydown", () => {
+    if (validKeys.includes(event.key)) {
+        showOnDisplay(event.key);
+    }
+    else if (event.key === "Delete") {
+        clearScreen();
+    }
+    else if (event.key === "Backspace") {
+        del();
+    }
+    else if (event.key === "Enter") {
+        equalsOperation();
+    }
+});
